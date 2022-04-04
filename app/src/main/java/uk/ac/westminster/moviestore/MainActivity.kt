37026129619eaddb62fb4c503718ddf637055509
Pramuditha Karunarathna <shemil076@gmail.com>
2022-04-04 10:13:52 +0530

    Started the project movie store

---
+++ app/src/main/java/uk/ac/westminster/moviestore/MainActivity.kt
@@ -0,0 +1,11 @@
+package uk.ac.westminster.moviestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

0ad785d86b07a203babf7d9e06e6f24aa703a590
Pramuditha Karunarathna <shemil076@gmail.com>
2022-04-13 12:13:34 +0530

    :card_file_box: implemented the database class

--- app/src/main/java/uk/ac/westminster/moviestore/MainActivity.kt
+++ app/src/main/java/uk/ac/westminster/moviestore/MainActivity.kt
@@ -2,10 +2,14 @@ package uk.ac.westminster.moviestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uk.ac.westminster.moviestore.entities.Movie
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    
 }
--- app/src/main/java/uk/ac/westminster/moviestore/MovieDatabase.kt
+++ app/src/main/java/uk/ac/westminster/moviestore/MovieDatabase.kt
@@ -1,6 +1,8 @@
 package uk.ac.westminster.moviestore
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import uk.ac.westminster.moviestore.entities.Actor
 import uk.ac.westminster.moviestore.entities.Movie
@@ -15,4 +17,23 @@ import uk.ac.westminster.moviestore.entities.relations.ActorMovieCrossRef
     version = 1
 )
 abstract class MovieDatabase : RoomDatabase() {
+    abstract val movieDao: MovieDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MovieDatabase? = null
+
+        fun getInstance(context: Context): MovieDatabase {
+            synchronized(this){
+                return INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    MovieDatabase::class.java,
+                    "movie_db"
+                ).build().also{
+                    INSTANCE = it
+                }
+            }
+        }
+
+    }
 }
